aee554907388e6fbeb2f4c6c872daf7076b64b19
Filip Wachowiak <filipw01@interia.eu>
2020-12-10 10:18:12 +0100

    Add day 10 python solution

--- python/day10/task1.py
+++ python/day10/task1.py
@@ -3,7 +3,21 @@ from os.path import dirname
 
 def run():
     with open(f'{dirname(__file__)}/data.txt', 'r') as file:
-        lines = file.readlines()
+        adapters = set(map(int, file.read().split('\n')[:-1]))
+        current_adapter = 0
+        diff_1 = 0
+        diff_3 = 1
+        while True:
+            if current_adapter + 1 in adapters:
+                diff_1 += 1
+                current_adapter += 1
+            elif current_adapter + 2 in adapters:
+                current_adapter += 2
+            elif current_adapter + 3 in adapters:
+                diff_3 += 1
+                current_adapter += 3
+            else:
+                return diff_1 * diff_3
 
 
 if __name__ == '__main__':
--- python/day10/task2.py
+++ python/day10/task2.py
@@ -1,9 +1,20 @@
+from collections import defaultdict
 from os.path import dirname
 
 
 def run():
     with open(f'{dirname(__file__)}/data.txt', 'r') as file:
-        lines = file.readlines()
+        adapters = sorted(map(int, file.read().split('\n')[:-1]))
+        max_adapter = max(adapters)
+        results = defaultdict(lambda: 0)
+        results[0] += 1
+        for adapter in adapters:
+            temp_results = results.copy()
+            for result, count in results.items():
+                if result + 1 == adapter or result + 2 == adapter or result + 3 == adapter:
+                    temp_results[adapter] += temp_results[result]
+            results = temp_results
+        return results[max_adapter]
 
 
 if __name__ == '__main__':

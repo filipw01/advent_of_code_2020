8963f65b985116bd31c49c18f4c323e715450a63
Filip Wachowiak <filipw01@interia.eu>
2020-12-14 10:02:20 +0100

    Add python day 14 and day 13 part 1

--- python/day13/task2.py
+++ python/day13/task2.py
@@ -3,7 +3,23 @@ from os.path import dirname
 
 def run():
     with open(f'{dirname(__file__)}/data.txt', 'r') as file:
-        lines = file.readlines()
+        data = file.read().split('\n')[1].split(',')
+        busses = []
+        # Use Chinese Reminder Theorem (all bus numbers are pairwise coprime)
+        for (delay, bus) in enumerate(data):
+            if bus == 'x':
+                continue
+            busses.append([int(bus), int(bus) - delay])
+        sorted(busses, key=lambda x: x[0], reverse=True)
+        index = 1
+        multiplier = busses[0][0]
+        prev = busses[0][1]
+        while index < len(busses):
+            prev = prev + multiplier
+            if busses[index][1] == prev % busses[index][0]:
+                multiplier *= busses[index][0]
+                index += 1
+        return prev
 
 
 if __name__ == '__main__':
--- python/day14/task1.py
+++ python/day14/task1.py
@@ -1,10 +1,36 @@
 from os.path import dirname
 
 
-def run():
+def emulate():
     with open(f'{dirname(__file__)}/data.txt', 'r') as file:
-        lines = file.readlines()
+        lines = file.read().split('\n')[:-1]
+        mask = ''
+        memory = {}
+        for line in lines:
+            (operation, value) = line.split(' = ')
+            if operation == 'mask':
+                mask = value
+            if operation[:3] == 'mem':
+                address = operation[4:-1]
+                memory[address] = apply_mask(value, mask)
+                print(f"mem[{address}] = mask_from {value} and {mask} = {apply_mask(value, mask)}")
+        return sum(memory.values())
+
+
+def apply_mask(value, mask):
+    value = str(bin(int(value))[2:])[::-1]
+    mask = mask[::-1]
+    output_number = ''
+    for index in range(len(mask)):
+        if mask[index] == 'X':
+            if index < len(value):
+                output_number += value[index]
+            else:
+                output_number += '0'
+        else:
+            output_number += mask[index]
+    return int(output_number[::-1], 2)
 
 
 if __name__ == '__main__':
-    print(run())
+    print(emulate())
